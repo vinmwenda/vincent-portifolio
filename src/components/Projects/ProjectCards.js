0c97f6bc4e3eeaeb03089bbc44f71c459ca988f5
Vincent Mwenda <vmwenda192@gmail.com>
2022-05-01 07:56:08 +0300

    portifolio

--- src/components/Projects/ProjectCards.js
+++ src/components/Projects/ProjectCards.js
@@ -4,16 +4,28 @@ import Button from "react-bootstrap/Button";
 import { BiLinkExternal } from "react-icons/bi";
 
 function ProjectCards(props) {
+  const handleClick = () => {
+    window.open("https://www.fakejumia.netlify.app");
+  };
+
   return (
     <Card className="project-card-view">
       <Card.Img variant="top" src={props.imgPath} alt="card-img" />
       <Card.Body>
-        <Card.Title style={{fontWeight: "bold"}}>{props.title}</Card.Title>
-        <Card.Text className="purple">Technology: {props.technologyUsed}</Card.Text>
+        <Card.Title style={{ fontWeight: "bold" }}>{props.title}</Card.Title>
+        <Card.Text className="purple">
+          Technology: {props.technologyUsed}
+        </Card.Text>
         <Card.Text style={{ textAlign: "justify" }}>
           {props.description}
         </Card.Text>
-        <Button variant="primary" href={props.link} target="_blank"className="download-button">
+        <Button
+          variant="primary"
+          href={props.link}
+          target="_blank"
+          className="download-button"
+          onClick={handleClick}
+        >
           <BiLinkExternal /> &nbsp;
           {props.isBlog ? "View Blog" : "View Project"}
         </Button>
